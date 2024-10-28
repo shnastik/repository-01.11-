2abcadf1ca5d9139745f933d65ca0802a33f9182
shnastik <agorbovtsova@inbox.ru>
2024-10-28 05:22:19 +0300

    Add files via upload

---
+++ task01.cpp
@@ -0,0 +1,253 @@
+/*
+    LIST 04 - TASK 01: BASIC OPERATIONS WITH VECTORS
+    Using functions and procedures, enter vectors A and B with dynamic memory
+    and let the user define the number of elements. Make the next operations:
+
+    [1]. Create a procedure that allows the user to enter vector elements from the keyboard.
+    [2]. Create a procedure that allows the user to print vector elements.
+    [3]. Calculate the Euclidean distance between two vectors.
+    [4]. Calculate the dot product of two vectors A and B using the linear combination formula.
+    [5]. Calculate the angle formed between vectors A and B.
+    [6]. Create a vector C that concatenates vectors A and B with elements sorted in ascending order. 
+         Use the insertion-sort algorithm.
+    [7]. Using the ordered vector C, calculate the statistical measures: 
+         - math expectation
+         - standard deviation
+         - median
+    
+    Definition of variables:
+        - n: number of elements of vector
+        - a, b: vectors
+        - c: vector result of unify a, b
+        - e_dist: output result of calculate euclidean distance
+        - s_prod: output result of calculate scalar product
+        - angle: output result of calculate angle between 2 vectors using functions with call by value
+        - angle2: output result of calculate angle between 2 vectors using procedure
+        - mean: output result of calculate mean of ordered vector C
+        - std: output result of calculate standard deviation of ordered vector C
+        - median: output result of calculate median of ordered vector C
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// header of functions defined by user
+void input_num_elements(int *n);
+int *allocate_memory(int n);
+void input_vector(int n, int *x);
+void print_vector(int n, int *x);
+double euclidean_dist(int n, int *x, int *y);
+double scalar_product(int n, int *x, int *y);
+double calculate_module(int n, int *x);
+double calculate_angle(int n, int *x, int *y);
+void calculate_angle2(int n, int *x, int *y, double *angle);
+void unify_vector(int n, int *x1, int *x2, int *y);
+void insertion_sort(int n, int *x);
+double calculate_mean(int n, int *x);
+double calculate_std(int n, int *x);
+double calculate_median(int n, int *x);
+
+// main function
+int main(){
+    // define variables
+    int n, *a, *b, *c;
+    double e_dist, s_prod, angle, angle2, mean, std, median;
+
+    // input the number of elements for vectors
+    input_num_elements(&n);
+
+    // dynamic allocation of memory
+    a = allocate_memory(n);
+    b = allocate_memory(n);
+
+    // input vectors A and B
+    cout<<"\nInput the elements for vectors: "<<endl;
+    cout<<"\nA: "<<endl;
+    input_vector(n, a);
+    cout<<"\nB: "<<endl;
+    input_vector(n, b);
+
+    // print elements of vectors A and B
+    cout<<"\nVector A: "<<endl;
+    print_vector(n, a);
+    cout<<"\nVector B: "<<endl;
+    print_vector(n, b);    
+
+    // calculate euclidean distance
+    e_dist = euclidean_dist(n, a, b);
+    cout<<"\nEuclidean distance = "<<e_dist<<endl;
+
+    // calculate scalar product
+    s_prod = scalar_product(n, a, b);
+    cout<<"\nScalar product = "<<s_prod<<endl;
+
+    // calculate angle using function by call
+    angle = calculate_angle(n, a, b);
+    cout<<"\nCalculate angle using function = "<<angle<<endl;
+
+    // calculate angle using procedure
+    calculate_angle2(n, a, b, &angle2);
+    cout<<"\nCalculate angle using process = "<<angle2<<endl;
+
+    // unify vectors a, b in vector c
+    c = allocate_memory(2*n);
+    unify_vector(n, a, b, c);
+    cout<<"\nUnified vector: "<<endl;
+    print_vector(2*n, c);
+
+    // apply sort in ascendent order
+    insertion_sort(2*n, c);
+    cout<<"\nSorted vector: "<<endl;
+    print_vector(2*n, c);
+
+    // print statistical results
+    mean = calculate_mean(2*n, c);
+    cout<<"\nMean = "<<mean<<endl;
+    std = calculate_std(2*n, c);
+    cout<<"\nStd = "<<std<<endl;
+    median = calculate_median(2*n, c);
+    cout<<"\nMedian = "<<median<<endl;
+
+    return 0;
+}
+
+// procedure to input the number of elements for vectors
+void input_num_elements(int *n){
+    do{
+        cout<<"\nNumber of elements: ";
+        cin>>*n;
+    }while(*n<=0);
+}   
+
+// function for dynamic allocation of memory for vectors using malloc
+int *allocate_memory(int n){
+    return (int *) malloc(n * sizeof(int));
+}
+
+// procedure to input the elements in 1 vector
+void input_vector(int n, int *x){
+    for(int i=0; i<n; i++){
+        cout<<"X["<<i<<"]: ";
+        cin>>x[i];
+    }
+}
+
+// procedure to print the elements of vector
+void print_vector(int n, int *x){
+    for(int i=0; i<n; i++){
+        cout<<x[i]<<"  ";
+    }
+    cout<<endl;
+}
+
+// function to calculate euclidean distance
+double euclidean_dist(int n, int *x, int *y){
+    double dist=0;
+
+    for(int i=0; i<n; i++){
+        dist+= pow(x[i] - y[i], 2);
+    }
+
+    return sqrt(dist);
+}
+
+// function to calculate scalar product
+double scalar_product(int n, int *x, int *y){
+    double sp=0;
+
+    for(int i=0; i<n; i++){
+        sp += x[i]*y[i];
+    }
+
+    return sp;
+}
+
+// function to calculate module of vector
+double calculate_module(int n, int *x){
+    double mod = 0;
+
+    for(int i=0; i<n; i++){
+        mod += pow(x[i], 2);
+    }
+
+    return sqrt(mod);
+}
+
+// function to calculate angle between 2 vectors
+double calculate_angle(int n, int *x, int *y){
+    double sp=0, mod_x=0, mod_y=0;
+
+    sp = scalar_product(n, x, y);
+    mod_x = calculate_module(n, x);
+    mod_y = calculate_module(n, y);
+
+    return acos(sp/(mod_x * mod_y));
+}
+
+// procedure to calculate angle between 2 vectors
+void calculate_angle2(int n, int *x, int *y, double *angle){
+    double sp=0, mod_x=0, mod_y=0;
+
+    sp = scalar_product(n, x, y);
+    mod_x = calculate_module(n, x);
+    mod_y = calculate_module(n, y);
+
+    *angle = acos(sp/(mod_x * mod_y));
+}
+
+// procedure to unify 2 vectors
+void unify_vector(int n, int *x1, int *x2, int *y){
+    for(int i=0; i<n; i++){
+        y[i] = x1[i];
+        y[i+n] = x2[i];
+    }
+}
+
+// algorithm insertion-sort in ascendent order
+void insertion_sort(int n, int *x){
+    int key, j;
+
+    for(int i=1; i<n; i++){
+        key = x[i];
+
+        for(j=i-1; j>=0 && x[j]>key; j--){
+            x[j+1] = x[j];
+        }
+
+        x[j+1] = key;
+    }
+}
+
+// function to calculate mean of vector C
+double calculate_mean(int n, int *x){
+    double sum = 0.0;
+
+    for(int i=0; i<n; i++){
+        sum += x[i];
+    }
+
+    return sum/n;
+}
+
+// function to calculate standard deviation of vector C
+double calculate_std(int n, int *x){
+    double mean=0.0, std=0.0;
+
+    mean = calculate_mean(n, x);
+
+    for(int i=0; i<n; i++){
+        std+= pow(x[i] - mean, 2);
+    }
+
+    return sqrt(std/n);
+}
+
+// function to calculate median of vector C
+double calculate_median(int n, int *x){
+    if(n%2 != 0){
+        return x[(n-1)/2];
+    }else{
+        return (x[n/2-1]+x[n/2])/2.0;
+    }   // one unit less than formula defined, because index in vector start in 0
+}
---
+++ task02.cpp
@@ -0,0 +1,148 @@
+/*
+    LIST 04 - TASK 02: OPERATIONS WITH VECTORS AND POINTERS
+    Enter a vector with dynamic memory allocation (uses the malloc function).
+    The user must dynamically determine the number of elements he wants to have
+    in the vector of integers. The vector elements must be in the range 1000 to 10000.
+    The number of vector elements must be even.
+
+    [1]. Check if the vector is STARKX. A vector is called STARKX if every number in the even field (0,2,4,...) 
+         is greater than any number in the odd field (1,3,5,...).
+    [2]. Extract the largest digit of each element of vector A. Generate a number whose magnitude order is 
+         the same as the index of the vector element.
+    [3]. Make a report of the number of digits that appear in each element of vector A. 
+         Store the number of digits in vector D and print the report of the number of digits.
+    [4]. Create a vector X that is a copy of vector A. Then create a function that checks the 
+         second smallest digit of each number in vector X. Modify each element of vector X by 
+         removing the second smallest digit (if there is a tie, remove the digit with the 
+         smallest ordinal number) and recounting each digit. 
+         Perform the digit count again on the new vector, and compare the digit count report in A 
+         with the report in X.
+
+    Definition of variables:
+        - n: number of elements of vector
+        - a: vector of numbers
+        - b: vector with max digits
+
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// header of functions defined by user
+void input_num_elements(int *n);
+int *allocate_memory(int n);
+void input_vector(int n, int *x);
+void print_vector(int n, int *x);
+bool is_starkx(int n, int *x);
+void generate_vec_max_digits(int n, int *x, int *y);
+int search_highest_digit(int num);
+
+// main function
+int main(){
+    // define variables
+    int n, *a, *b;
+
+    // input number of elements for vector
+    input_num_elements(&n);
+
+    // dynamic allocation of memory
+    a = allocate_memory(n);
+
+    // input the elements of vector A
+    cout<<"\nInput the elements in vector: "<<endl;
+    input_vector(n, a);
+    cout<<"\nVector A: "<<endl;
+    print_vector(n, a);
+
+    // check if vector is STARKX
+    if(is_starkx(n, a) == true){
+        cout<<"\nThe vector is STARKX"<<endl;
+    }else{
+        cout<<"\nThe vector is not STARKX"<<endl;
+    }
+
+    // generate vector with maximum digits
+    b = allocate_memory(n);
+    generate_vec_max_digits(n, a, b);
+    cout<<"\nVector of max digits: "<<endl;
+    print_vector(n, b);
+
+    return 0;
+}
+
+// procedure to input the number of elements for vectors
+void input_num_elements(int *n){
+    do{
+        cout<<"\nNumber of elements: ";
+        cin>>*n;
+    }while(*n<=0 || *n%2 != 0);
+}   
+
+// function for dynamic allocation of memory for vectors using malloc
+int *allocate_memory(int n){
+    return (int *) malloc(n * sizeof(int));
+}
+
+// procedure to input the elements in 1 vector
+void input_vector(int n, int *x){
+    for(int i=0; i<n; i++){
+        do{
+            cout<<"X["<<i<<"]: ";
+            cin>>x[i];
+        }while(x[i]<1000 || x[i]>10000);        
+    }
+}
+
+// procedure to print the elements of vector
+void print_vector(int n, int *x){
+    for(int i=0; i<n; i++){
+        cout<<x[i]<<"  ";
+    }
+    cout<<endl;
+}
+
+// function to check if vector is STARKX
+bool is_starkx(int n, int *x){
+    int *even, *odd, counter=0;
+    bool flag=true;
+    even = x;
+    odd = x;
+
+    while(counter < n/2 && flag == true){
+        if(*(even+2*counter) < *(odd+2*counter+1)){ // move 2 positions to right
+            flag = false;
+        }
+
+        counter++;
+    }
+
+    return flag;
+}
+
+// procedure to generate vector with max digits
+void generate_vec_max_digits(int n, int *x, int *y){
+    for(int i=0; i<n; i++){
+        y[i] = search_highest_digit(x[i]);
+    }
+}
+
+// function to return the max digit for 1 number
+int search_highest_digit(int num){
+    int aux, dig, max_dig=-1;
+    aux = num;
+
+    // decompose the number
+    while(aux>0){
+        dig = aux%10;
+
+        if(dig>max_dig){    // check if have higher digit than currently max_digit
+            max_dig = dig;
+        }
+
+        aux = aux/10;
+    }
+
+    return max_dig;
+}
+
